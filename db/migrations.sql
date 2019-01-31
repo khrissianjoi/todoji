a631173b9d887fa4c695ab75522b4400c827cd95
Ilie Cebanu <ilie@purpledecks.com>
2019-01-31 23:31:11 +0000

    Add more comments to migrations file

--- db/migrations.sql
+++ db/migrations.sql
@@ -1,7 +1,13 @@
 -- These we will run manually to update the DB schema as we go along.
 -- Either parts of the file can be applied, or the complete file
 -- can be loaded if starting from nothing.
+--
+-- The reason this is in 1 file instead of being broken into many,
+-- is to simplify git merging and loading it in.
 
+
+-- Migration 1
 CREATE DATABASE todoji;
 
 USE todoji;
+-- End of Migration 1
