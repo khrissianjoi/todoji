f8fabf51e9d5d6aae3a69091baec1a519296d64a
Ilie Cebanu <ilie@purpledecks.com>
2019-01-31 23:25:59 +0000

    Add comment explaining migrations file

--- db/migrations.sql
+++ db/migrations.sql
@@ -1,3 +1,7 @@
+-- These we will run manually to update the DB schema as we go along.
+-- Either parts of the file can be applied, or the complete file
+-- can be loaded if starting from nothing.
+
 CREATE DATABASE todoji;
 
 USE todoji;
