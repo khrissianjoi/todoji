2229812a76c0dca507e673862dde1d4ee7b3471e
Ilie Cebanu <ilie@purpledecks.com>
2019-02-19 18:35:25 +0000

    Clean up getTasks controller

--- controllers/task.js
+++ controllers/task.js
@@ -6,46 +6,44 @@ const task_model = require('../models/tasks');
 module.exports = {
   async postTask(req, res, next) {
     try {
-        const schema_errors = post_task_schema(req.body);
-        if (schema_errors) {
-            throw http_errors(400, schema_errors);
+      const schema_errors = post_task_schema(req.body);
+      if (schema_errors) {
+        throw http_errors(400, schema_errors);
       }
       await task_model.createTask(req.body);
       res.status(200).end();
     } catch (error) {
-        next(error);
+      next(error);
     }
-	},
+  },
 
   async getTasks(req, res, next) {
     try {
-      if ('folder_id' in req.query)
-      {
-        const user_id = parseInt(req.query.user_id, 10);
-        const folder_id = parseInt(req.query.folder_id, 10);
-        if (folder_id < 0 || user_id < 0) {
-        throw http_errors(400, 'invalid user id or folder id');
-        }
-        const tasks = await task_model.getTask_UserFolder(user_id, folder_id);
-        res.json(tasks);
+      const user_id = parseInt(req.query.user_id, 10);
+      const folder_id = parseInt(req.query.folder_id, 10);
+      if (userId < 0) {
+        throw http_errors(400, 'invalid user id');
       }
-      else {
-        const userId = parseInt(req.query.id, 10);
-        if (userId < 0) {
-          throw http_errors(400, 'invalid folder id');
+
+      let tasks;
+      if (req.query.folder_id) {
+        if (folder_id < 0) {
+          throw http_errors(400, 'invalid  folder id');
         }
-        const tasks = await task_model.getTasks(userId);
-        res.json({
-          items : tasks
-        });
+        tasks = await task_model.getTask_UserFolder(user_id, folder_id);
+      } else {
+        tasks = await task_model.getTasks(userId);
       }
+
+      res.json({
+        items: tasks,
+      });
     } catch (error) {
       next(error);
     }
   },
   async getTask(req, res, next) {
     try {
- 
       const taskId = parseInt(req.query.id, 10);
       if (taskId < 0) {
         throw http_errors(400, 'invalid task id');
@@ -55,6 +53,5 @@ module.exports = {
     } catch (error) {
       next(error);
     }
-  }
+  },
 };
-	
